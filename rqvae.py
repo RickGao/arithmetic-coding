2742e20d5a3b1fd95ae2d30db33b9e5e7ba66d44
RickGao <rickgaoid@outlook.com>
2025-10-25 00:34:54 -0400

    Update

--- rqvae.py
+++ rqvae.py
@@ -20,25 +20,29 @@ def readcode(filename, n=None):
 training_sequences = readcode('codes23x40x4.txt', 900)
 
 # model = NGramModel(n=1, k=0.1, start_token=-1, end_token=-2, initial_vocab=set(range(2048)))
-model = NGramModel(n=3, k=0.1, start_token=-1, end_token=-2, initial_vocab=set(range(2048)))
+# model = NGramModel(n=3, k=0.1, start_token=-1, end_token=-2, initial_vocab=set(range(2048)))
 
 
-model.fit(training_sequences)
+# model.fit(training_sequences)
 
 
-prob_dist = model.get_probability_distribution()
-with open('prob_dict.pkl', 'wb') as f:
-    pickle.dump(prob_dist, f)
+# prob_dist = model.get_probability_distribution()
+# with open('prob_dict.pkl', 'wb') as f:
+#     pickle.dump(prob_dist, f)
 
-# with open('prob_dict.pkl', 'rb') as f:
-#     prob_dist = pickle.load(f)
+with open('prob_dict.pkl', 'rb') as f:
+    prob_dist = pickle.load(f)
+
+print(type(prob_dist))
 
 # inner_dict = prob_dist[(1498,)]
 # total = sum(inner_dict.values())
 # print(total)
 
 
-encoder = ArithmeticEncoder(ngram_model=model, bits=32)
+encoder = ArithmeticEncoder(prob_distribution=prob_dist, bits=32)
+
+print("Encoder Created")
 
 test_sequence = readcode('codes23x40x4.txt', 1000)[990][:3000]
 
@@ -48,7 +52,7 @@ encoded_bits = encoder.encode(test_sequence)
 print(f"编码后: {len(encoded_bits)} bits")
 print(f"压缩率: {len(encoded_bits) / (len(test_sequence) * 11):.2%}")
 
-# 步骤4: 解码验证
+
 decoded_sequence = encoder.decode(encoded_bits)
 # print(f"解码结果: {decoded_sequence}")
 print(f"正确性: {'正确' if decoded_sequence == test_sequence else '错误'}")
